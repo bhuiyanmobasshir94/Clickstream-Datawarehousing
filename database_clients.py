8ce513aa9c2a58dd0df58acf0c50584edfa3a544
bhuiyanmobasshir94 <mobasshir.bhuiya@graaho.com>
2021-11-07 21:11:33 +0600

    Created database clients and updated gitignore, requirements

---
+++ database_clients.py
@@ -0,0 +1,105 @@
+import os
+import sqlite3
+import sys
+
+import psycopg2
+
+
+class PostgreSQLClient:
+    def __init__(self, dbname, user, password):
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+        self.conn = None
+        self.cursor = None
+
+    def connect(self):
+        try:
+            self.conn = psycopg2.connect(
+                dbname=self.dbname, user=self.user, password=self.password
+            )
+            self.cursor = self.conn.cursor()
+            return self.conn
+        except psycopg2.Error as e:
+            print("Error connecting to database: ", e)
+            sys.exit(1)
+
+    def disconnect(self):
+        if self.conn:
+            self.conn.close()
+
+    def execute(self, query, params=None):
+        if not self.cursor:
+            self.connect()
+        try:
+            self.cursor.execute(query, params)
+            self.conn.commit()
+        except psycopg2.Error as e:
+            print("Error executing query: ", e)
+            sys.exit(1)
+
+    def fetch_all(self):
+        if not self.cursor:
+            self.connect()
+        try:
+            return self.cursor.fetchall()
+        except psycopg2.Error as e:
+            print("Error fetching results: ", e)
+            sys.exit(1)
+
+    def fetch_one(self):
+        if not self.cursor:
+            self.connect()
+        try:
+            return self.cursor.fetchone()  # returns a tuple
+        except psycopg2.Error as e:
+            print("Error fetching results: ", e)
+            sys.exit(1)
+
+
+class SQLiteClient:
+    def __init__(self, dbname):
+        self.dbname = dbname
+        self.conn = None
+        self.cursor = None
+
+    def connect(self):
+        try:
+            self.conn = sqlite3.connect(self.dbname)
+            self.cursor = self.conn.cursor()
+            return self.conn
+        except sqlite3.Error as e:
+            print("Error connecting to database: ", e)
+            sys.exit(1)
+
+    def disconnect(self):
+        if self.conn:
+            self.conn.close()
+
+    def execute(self, query):
+        if not self.cursor:
+            self.connect()
+        try:
+            self.cursor.execute(query)
+            self.conn.commit()
+        except sqlite3.Error as e:
+            print("Error executing query: ", e)
+            sys.exit(1)
+
+    def fetch_all(self):
+        if not self.cursor:
+            self.connect()
+        try:
+            return self.cursor.fetchall()
+        except sqlite3.Error as e:
+            print("Error fetching results: ", e)
+            sys.exit(1)
+
+    def fetch_one(self):
+        if not self.cursor:
+            self.connect()
+        try:
+            return self.cursor.fetchone()  # returns a tuple
+        except sqlite3.Error as e:
+            print("Error fetching results: ", e)
+            sys.exit(1)
