75c456c7a8acbd6608353c606437bec4ec8977c2
bhuiyanmobasshir94 <mobasshirbhuiyan.shagor@gmail.com>
2021-11-08 14:23:19 +0600

    database_clients updated

--- populate_datawarehouse.py
+++ populate_datawarehouse.py
@@ -2,21 +2,47 @@ import os
 from datetime import datetime
 
 import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
-from database_clients import SQLiteClient
+from database_clients import PostgreSQLClient, SQLiteClient
 
-sqlite = SQLiteClient("datawarehouse.sqlite")
-sqlite_engine = sqlite.connect()
+load_dotenv()
+
+USE_POSTGRESQL = os.getenv("USE_POSTGRESQL") == "True"
+POSTGRESQL_HOST = os.getenv("POSTGRESQL_HOST")
+POSTGRESQL_PORT = int(os.getenv("POSTGRESQL_PORT"))
+POSTGRESQL_USER = os.getenv("POSTGRESQL_USER")
+POSTGRESQL_PASSWORD = os.getenv("POSTGRESQL_PASSWORD")
+POSTGRESQL_DB = os.getenv("POSTGRESQL_DB")
+
+if USE_POSTGRESQL:
+    print("Using PostgreSQL")
+    client = PostgreSQLClient(
+        dbname=POSTGRESQL_DB,
+        user=POSTGRESQL_USER,
+        password=POSTGRESQL_PASSWORD,
+        host=POSTGRESQL_HOST,
+        port=POSTGRESQL_PORT,
+    )
+    client.connect()
+    client_engine = create_engine(
+        f"postgresql://{POSTGRESQL_USER}:{POSTGRESQL_PASSWORD}@{POSTGRESQL_HOST}:{POSTGRESQL_PORT}/{POSTGRESQL_DB}"
+    )
+else:
+    print("Using SQLite")
+    client = SQLiteClient("datawarehouse.sqlite")
+    client_engine = client.connect()
 
 condition = ("2017", "2021")  # year range
 
 datacatalog_number_of_rows = pd.read_sql(
-    "SELECT COUNT(*) FROM datacatalog", sqlite_engine
+    "SELECT COUNT(*) FROM datacatalog", client_engine
 ).iloc[0, 0]
 
 if datacatalog_number_of_rows > 0:
     print("Datacatalog already populated.")
-    datacatalog = pd.read_sql("SELECT * FROM datacatalog", sqlite_engine)
+    datacatalog = pd.read_sql("SELECT * FROM datacatalog", client_engine)
     columns = ["referrer", "resource", "type", "number_of_occurrences"]
 
     for index, row in datacatalog.iterrows():
@@ -34,25 +60,35 @@ if datacatalog_number_of_rows > 0:
             file_path = os.path.join("data", folder_name, file_name)
             if os.path.isfile(file_path):
                 print(f"Processing {file_name} to ingest.")
-                df = pd.read_table(file_path, compression="gzip")
+                # df = pd.read_table(file_path, compression="gzip")
+                df = pd.read_table(
+                    file_path,
+                    compression="gzip",
+                    usecols=range(0, 4),
+                    header=None,
+                    low_memory=False,
+                )
                 df.columns = columns
                 df["Date"] = str(datetime.strptime(folder_name, "%Y-%m").date())
                 df.to_sql(
                     name="clickstream",
-                    con=sqlite_engine,
+                    con=client_engine,
                     if_exists="append",
                     index=False,
                 )
                 # data_catalog.loc[index, "data_inserted"] = True
-                sqlite.execute(
+                client.execute(
                     f"UPDATE datacatalog SET data_inserted = True WHERE file_name = '{file_name}'"
                 )
                 # data_catalog.loc[index, "number_of_rows"] = df.shape[0]
-                sqlite.execute(
+                client.execute(
                     f"UPDATE datacatalog SET number_of_rows = {df.shape[0]} WHERE file_name = '{file_name}'"
                 )
                 print(f"{df.shape[0]} rows ingested from {file_name}.")
+
+                if index % 10 == 0:
+                    print(f"At index {index}.")
 else:
     import create_datalake
 
-sqlite.disconnect()
+client.disconnect()
