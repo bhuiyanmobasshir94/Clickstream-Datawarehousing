084cfbba94f19ea463567cdffbe22b8088f26b58
bhuiyanmobasshir94 <mobasshirbhuiyan.shagor@gmail.com>
2021-11-10 12:29:57 +0600

    Cinfig added and improved and utilities deleted

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 import os
 import ssl
+import warnings
 
 from dotenv import load_dotenv
 
+warnings.filterwarnings("ignore")
 ssl._create_default_https_context = ssl._create_unverified_context
 load_dotenv()
 
 ## Get the details from the .env file
+DATA_VENDOR_URL = os.getenv("DATA_VENDOR_URL")
 USE_POSTGRESQL = os.getenv("USE_POSTGRESQL") == "True"
 POSTGRESQL_HOST = os.getenv("POSTGRESQL_HOST")
 POSTGRESQL_PORT = int(os.getenv("POSTGRESQL_PORT"))
@@ -14,6 +17,7 @@ POSTGRESQL_USER = os.getenv("POSTGRESQL_USER")
 POSTGRESQL_PASSWORD = os.getenv("POSTGRESQL_PASSWORD")
 POSTGRESQL_DB = os.getenv("POSTGRESQL_DB")
 USE_DATALAKE = os.getenv("USE_DATALAKE") == "True"
+CREATE_DATALAKE = os.getenv("CREATE_DATALAKE") == "True"
 DATALAKE_DIR = os.getenv("DATALAKE_DIR")
 DATACATALOG_TABLE = os.getenv("DATACATALOG_TABLE")
 DATAWAREHOUSE_TABLE = os.getenv("DATAWAREHOUSE_TABLE")
@@ -29,3 +33,11 @@ FILE_NAMES = (
 )
 USE_DF_CHUNK = os.getenv("USE_DF_CHUNK") == "True"
 USE_DF_CHUNK_SIZE = int(os.getenv("USE_DF_CHUNK_SIZE"))
+LOG_DIR = os.getenv("LOG_DIR")
+LOG_FILE_NAME = os.getenv("LOG_FILE_NAME")
+
+if os.path.exists(LOG_DIR) is False:
+    os.makedirs(LOG_DIR)
+
+if os.path.exists(DATALAKE_DIR) is False:
+    os.makedirs(DATALAKE_DIR)
